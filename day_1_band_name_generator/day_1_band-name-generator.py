e2563570e96bd3a2b68287c56bfccafbd67914f1
JKUOL <koenigjustin7@gmail.com>
2023-11-13 18:48:49 +0100

    initial commit

---
+++ day_1_band_name_generator/day_1_band-name-generator.py
@@ -0,0 +1,11 @@
+# band-name-generator
+
+
+# input( will get user input in console)
+x = input("What is the name of your hometown?\n")
+y = input("Whats the name of your first pet?\n")
+
+# print outputs the band name by combination of hometown x and pet name y
+print("The name of your band is " + x + " " + y)
+
+
